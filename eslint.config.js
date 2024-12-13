f45e6fdbc7dec06c29fe09153a1e2ba7bb3475ca
Maxim Veremeenko <mvv@vt2b.ru>
2024-12-14 02:20:30 +0500

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  { languageOptions: { globals: globals.browser } },
+  eslintConfigPrettier,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': ['warn'],
+      'react/react-in-jsx-scope': 'off',
+      // 'no-extra-semi': 'on',
+    },
+    ignores: ['.src/components/ui-shadcn/*', './tailwind.config.js'],
+  },
+];
